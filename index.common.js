c202036dceaa62b06f7d6b33175144321d0962f2
Nick Bobryk <nick.bobryk@vydia.com>
2016-11-07 20:33:57 -0500

    initial

---
+++ index.common.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import App from './App';
+
+class Camra extends React.Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+AppRegistry.registerComponent('Camra', () => Camra);
